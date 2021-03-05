81358e3c98e19547603cf58bc2cff8a761294e43
Jason Man <jason202005@gmail.com>
2021-03-06 02:55:02 +0800

    header javascript & html done

--- wapp.js
+++ wapp.js
@@ -1,6 +1,7 @@
 headerHTML();
 headerData();
 
+
 function headerData(){
     fetch('https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=rhrread&lang=en')
         .then( response => {
@@ -10,7 +11,7 @@ function headerData(){
             let TempData = gettingData.temperature.data[1];
             output = '<span class="temperture"> '+ TempData.value + TempData.unit + ' </span>'
             document.getElementsByClassName("header Temperature")[0].innerHTML = output;
-            
+            // Ref: https://stackoverflow.com/questions/34456436/document-getelementsbyclassname-innerhtml-not-working
             TempData = gettingData.icon[0];
             let iconurl = `https://www.hko.gov.hk/images/HKOWxIconOutline/pic${TempData}.png`;
             output = '<img src="'+ iconurl +'"> </span>'
@@ -24,6 +25,13 @@ function headerData(){
             TempData = gettingData.rainfall.data[13];
             output = '<span class="rainfall"> '+ TempData.max + TempData.unit + ' </span>';
             document.getElementsByClassName("header Rainfall")[0].innerHTML = output;
+            if (rainingChecker(TempData.max) == 1) {
+                document.getElementsByTagName("header")[0].classList.add("raining");
+            } else {
+                document.getElementsByTagName("header")[0].classList.add("withoutRaining");
+            }
+
+
             let mayMissingData;
             try {
                 mayMissingData = gettingData.uvindex.data[0].value;
@@ -36,10 +44,17 @@ function headerData(){
                 document.getElementsByClassName("header UVLevel")[0].innerHTML = output;
             }
            
-            TempData = gettingData.updateTime;
-            output = '<span class="lastupdate">' + TempData + '</span>';
+            TempData = gettingData.updateTime.substr(11,5);
+            // var hour = gettingData.updateTime.substr(11,2);
+            var hour = new Date();
+            if (daytimeChecker(hour.getHours()) == 1){
+                document.getElementsByTagName("header")[0].classList.add("day");
+            }else{
+                document.getElementsByTagName("header")[0].classList.add("night");
+            }
+            output = '<span class="lastupdate"> Last Update: ' + TempData + '</span>';
             document.getElementsByClassName("header LastUpdate")[0].innerHTML = output;
-            
+
             try {
                 mayMissingData = warningMessage;
                 output = '<span class="warning">' + mayMissingData  + '</span>';
@@ -60,3 +75,16 @@ function headerHTML() {
     document.body.innerHTML = '<header> <div class="header title"> <h1>My Weather Portal</h1></div> <div class="header block"> <div class="header location">Hong Kong</div> <div class="header WeatherIcon"></div> <div class="header Temperature"></div> <div class="header Humidity"></div> <div class="header Rainfall"></div> <div class="header UVLevel"></div> <div class="header LastUpdate"></div> <div class="header Warning"></div> </div> </<header> <div id="output" style="margin-top: 1rem"></div>';
 }
 
+function rainingChecker(volume) {
+    if ( volume > 0 ) return 1;
+    else return 0;
+}
+
+function daytimeChecker(hour){
+    if (parseInt(hour) > 5 && parseInt(hour) < 17){
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
